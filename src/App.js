5863708c8840d476c19b2204541c597263ea5c2f
adriank <adriankhoo@hotmail.com>
2021-06-01 01:19:14 +0800

    First draft

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useRef, useEffect } from 'react';
+import { useLocation, Switch } from 'react-router-dom';
+import AppRoute from './utils/AppRoute';
+import ScrollReveal from './utils/ScrollReveal';
+
+// Layouts
+import LayoutDefault from './layouts/LayoutDefault';
+
+// Views 
+import Home from './views/Home';
+
+const App = () => {
+
+  const childRef = useRef();
+  let location = useLocation();
+
+  useEffect(() => {
+    document.body.classList.add('is-loaded')
+    childRef.current.init();
+  }, [location]);
+
+  return (
+    <ScrollReveal
+      ref={childRef}
+      children={() => (
+        <Switch>
+          <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
+        </Switch>
+      )} />
+  );
+}
+
+export default App;
